ece74e49da3c1e7bf82d06dd86523eba38b9109d
ThomasKonan <thomkonan@gmail.com>
2025-11-10 15:43:59 -0500

    first commit, framework, ThemeToggle, StarBackground

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
