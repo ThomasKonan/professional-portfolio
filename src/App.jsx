92582d1554e1dd98c16b2c48940f9b1f4d519ed5
ThomasKonan <thomkonan@gmail.com>
2025-11-11 16:56:31 -0500

    Adde Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
+
 function App() {
   return (
     <>
